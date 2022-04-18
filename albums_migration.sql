3354468be81d3a1651d96348421eef0d11ff60c4
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-04-18 16:52:00 -0500

    completed select exercises portion of basic statements exercises

--- albums_migration.sql
+++ albums_migration.sql
@@ -12,3 +12,24 @@ CREATE TABLE albums (
     PRIMARY KEY (id)
 );
 
+INSERT INTO albums(artist, name, release_date, sales, genre) VALUES ('J cole','2014 Forest Hills Drive',2014,5000000,'Rap');
+
+INSERT INTO albums(artist, name, release_date, sales, genre) VALUES ('Eminem','an album',2000,100000,'Rap');
+
+INSERT INTO albums(artist, name, release_date, sales, genre) VALUES ('Kendrick Lamar','an album',2014,3000000,'Rap');
+
+INSERT INTO albums(artist, name, release_date, sales, genre) VALUES ('The Weeknd','Trilogy',2014,2000000,'Rap');
+
+INSERT INTO albums(artist, name, release_date, sales, genre) VALUES ('Childish Gambino','2nd album',2012,2000000,'Rap');
+
+INSERT INTO albums(artist, name, release_date, sales, genre)
+            VALUES('1st artist','their album',2005,1500000,'Rock'),
+                  ('Linkin Park','Numb',2004,2000000,'Alternative'),
+                  ('3rd artist','The album',2007,800000,'Rock');
+
+SELECT * FROM albums WHERE release_date > 2010;
+
+DESCRIBE albums;
+
+SHOW CREATE TABLE albums;
+
---
+++ select_exercises.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT release_date From albums Where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT genre FROM albums WHERE name = 'Nevermind';
+
+SELECT name FROM albums WHERE release_date >= 1990 && albums.release_date < 2000;
+
+SELECT name FROM albums WHERE sales < 20000000;
+
+SELECT name FROM albums WHERE genre = 'Rock';

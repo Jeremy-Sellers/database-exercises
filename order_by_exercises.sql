d23f89d98e2f4c4c90e9e27af6e3f7d1fc0e27b6
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-04-19 15:41:36 -0500

    completed order by exercise

---
+++ order_by_exercises.sql
@@ -0,0 +1,42 @@
+USE employees;
+
+#Find all employees with first names Irena, Vidya, Maya
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena','Vidya','Maya')
+ORDER BY last_name, first_name;
+
+# Find all employees whose last name starts with E
+SELECT *
+FROM employees
+WHERE last_name LIKE ('E%');
+
+# Find all employees with a q in their last name
+SELECT *
+FROM employees
+WHERE last_name like ('%q%');
+
+# Update first query to use OR instead of IN and whose gender is male
+SELECT *
+FROM employees
+WHERE gender = 'M' AND
+    (first_name = 'Irena' OR
+     first_name = 'Vidya' OR
+     first_name = 'Maya');
+
+# Find all employees whose last name starts OR ends with E
+SELECT *
+FROM employees
+WHERE last_name like ('%E') or last_name like ('E%')
+ORDER BY emp_no DESC;
+
+# Find all employees whose last name starts AND ends with E
+SELECT *
+FROM employees
+WHERE last_name like ('E%E');
+
+# Find all employees with a q in their lsat name but not qu
+SELECT *
+FROM employees
+WHERE last_name like ('%q%') And last_name not like ('%qu%');
+

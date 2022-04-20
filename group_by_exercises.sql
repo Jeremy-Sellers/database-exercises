d2ad8a7c3e90fbdb03f9bb196e11bbc9762e39e3
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-04-20 13:55:29 -0500

    updated output of last two exercises to display in correct order

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -25,13 +25,13 @@ WHERE last_name like ('%q%') And last_name not like ('%qu%')
 group by last_name;
 
 # Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
-SELECT last_name, count(last_name)
+SELECT count(last_name), last_name
 FROM employees
 WHERE last_name like ('%q%') And last_name not like ('%qu%')
 group by last_name;
 
 # Update your query for 'Irena', 'Vidya', or 'Maya'. Use count(*) and GROUP BY to find the number of employees for each gender with those names.
-SELECT gender, count(*)
+SELECT count(*), gender
 FROM employees
 WHERE first_name IN ('Irena','Vidya','Maya')
 group by gender;
